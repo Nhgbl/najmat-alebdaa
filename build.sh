2ddee48cf2548357dfd9e4674e0d5b2064f0f09c
Nhgbl <nhgbl@test.com>
2026-03-06 03:16:55 +0300

    تفعيل ميزة إنشاء المدير تلقائياً

--- build.sh
+++ build.sh
@@ -6,3 +6,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# إنشاء حساب المدير تلقائياً إذا لم يكن موجوداً
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser \
+        --no-input \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
